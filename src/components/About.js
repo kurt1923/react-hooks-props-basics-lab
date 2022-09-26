7d132f5d531b003516bc9f8ecd5393daff4c1df0
Kurt Vermillion <kurtv0727@gmail.com>
2022-09-25 23:00:10 -0400

    lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-function About() {
+
+function About(props) {
+  const hasBio = props.bio
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{ hasBio ? props.bio : null}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <h3>Links</h3>
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}>{props.linkedin}</a>
+      
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color= {user.color} name = {user.name} city={user.city}  />
+      <About hasBio= {true} github= {user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
 }
